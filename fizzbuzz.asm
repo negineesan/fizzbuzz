c2f1ec9e80eb4592235995d5c5e1bca7456959db
negineesan <negineesan@gmail.com>
2025-03-01 20:14:11 +0900

    Fix: Use printf instead of WriteConsoleA

--- fizzbuzz.asm
+++ fizzbuzz.asm
@@ -1,35 +1,18 @@
 .code
 extrn ExitProcess: proc
-extrn GetStdHandle: proc
-extrn WriteConsoleA: proc
-extrn GetLastError: proc
+extrn printf: proc
 
 .data
-fizz_str BYTE "Fizz", 0
-buzz_str BYTE "Buzz", 0
-fizzbuzz_str BYTE "FizzBuzz", 0
-newline BYTE 0Dh, 0Ah, 0
-start_msg BYTE "Start_FizzBuzz...", 0Dh, 0Ah, 0
-std_out_handle QWORD ?
-bytes_written QWORD ?
-buffer BYTE 12 DUP(0)
+format_str BYTE "%d", 0Dh, 0Ah, 0
+fizz_str BYTE "Fizz", 0Dh, 0Ah, 0
+buzz_str BYTE "Buzz", 0Dh, 0Ah, 0
+fizzbuzz_str BYTE "FizzBuzz", 0Dh, 0Ah, 0
 
 .code
 main proc
     push rbp
     mov rbp, rsp
     sub rsp, 32
-
-    mov rcx, -11
-    call GetStdHandle
-    mov std_out_handle, rax
-
-    lea rdx, start_msg
-    mov rcx, std_out_handle
-    mov r8, sizeof start_msg - 1
-    lea r9, bytes_written
-    push 0
-    call WriteConsoleA
 
     mov rsi, 1
 
@@ -58,26 +41,29 @@ loop_start:
     cmp rdx, 0
     je print_buzz
 
-    jmp print_number
+    ; 数値をprintfで出力
+    mov rcx, offset format_str
+    mov rdx, rsi
+    call printf
+
+    jmp loop_continue
 
 print_fizzbuzz:
-    call write_fizzbuzz
-    jmp print_newline
+    mov rcx, offset fizzbuzz_str
+    call printf
+    jmp loop_continue
 
 print_fizz:
-    call write_fizz
-    jmp print_newline
+    mov rcx, offset fizz_str
+    call printf
+    jmp loop_continue
 
 print_buzz:
-    call write_buzz
-    jmp print_newline
+    mov rcx, offset buzz_str
+    call printf
+    jmp loop_continue
 
-print_number:
-    call write_number
-    jmp print_newline
-
-print_newline:
-    call write_newline
+loop_continue:
     inc rsi
     jmp loop_start
 
@@ -86,113 +72,6 @@ loop_end:
     pop rbp
     mov rcx, 0
     call ExitProcess
-
-write_fizzbuzz proc
-    sub rsp, 32
-    mov rcx, std_out_handle
-    lea rdx, fizzbuzz_str
-    mov r8, sizeof fizzbuzz_str - 1
-    lea r9, bytes_written
-    push 0
-    call WriteConsoleA
-    add rsp, 32
-    ret
-write_fizzbuzz endp
-
-write_fizz proc
-    sub rsp, 32
-    mov rcx, std_out_handle
-    lea rdx, fizz_str
-    mov r8, sizeof fizz_str - 1
-    lea r9, bytes_written
-    push 0
-    call WriteConsoleA
-    add rsp, 32
-    ret
-write_fizz endp
-
-write_buzz proc
-    sub rsp, 32
-    mov rcx, std_out_handle
-    lea rdx, buzz_str
-    mov r8, sizeof buzz_str - 1
-    lea r9, bytes_written
-    push 0
-    call WriteConsoleA
-    add rsp, 32
-    ret
-write_buzz endp
-
-write_newline proc
-    sub rsp, 32
-    mov rcx, std_out_handle
-    lea rdx, newline
-    mov r8, sizeof newline - 1
-    lea r9, bytes_written
-    push 0
-    call WriteConsoleA
-    add rsp, 32
-    ret
-write_newline endp
-
-write_number proc
-    sub rsp, 32
-    mov rax, rsi
-    call convert_to_string
-
-    mov rdx, rax
-    mov rcx, std_out_handle
-    mov r8, r10
-    lea r9, bytes_written
-    push 0
-    call WriteConsoleA
-    add rsp, 32
-    ret
-write_number endp
-
-convert_to_string proc
-    push rcx
-    sub rsp, 32
-    lea rdi, buffer
-    mov rcx, 0
-
-    mov rbx, 10
-    test rax, rax
-    jnz convert_loop
-    mov byte ptr [rdi], '0'
-    inc rcx
-    mov rax, rdi
-    jmp convert_done
-
-convert_loop:
-    xor rdx,rdx
-    div rbx
-    add dl, '0'
-    mov [rdi+rcx], dl
-    inc rcx
-    test rax,rax
-    jnz convert_loop
-
-    mov rax, rdi
-    lea rbx, [rdi+rcx-1]
-reverse_loop:
-    cmp rdi,rbx
-    jge convert_done
-    mov dl,[rdi]
-    mov dh,[rbx]
-    mov [rdi],dh
-    mov [rbx],dl
-    inc rdi
-    dec rbx
-    jmp reverse_loop
-
-convert_done:
-    mov byte ptr [rdi+rcx], 0
-    mov rax, rdi
-    mov r10, rcx
-    pop rcx
-    ret
-convert_to_string endp
 
 main endp
 end
